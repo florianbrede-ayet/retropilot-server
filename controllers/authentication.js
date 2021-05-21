a5fbb941622ad1241852c435ff5582a98622f4e5
Florian Brede <florian.brede@ayetstudios.com>
2021-05-21 17:41:38 +0200

    FIX: typo in jwt token validation

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -3,9 +3,9 @@ const jwt = require('jsonwebtoken');
 
 
 
-async function validateJWT(JWT, key) {
+async function validateJWT(token, key) {
     try {
-        return jwt.verify(jwt.replace("JWT ", ""), key, {algorithms: ['RS256']});
+        return jwt.verify(token.replace("JWT ", ""), key, {algorithms: ['RS256']});
     } catch (exception) {
         // TODO add logger to authentication controller
         //logger.error(exception);
