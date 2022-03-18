cd46118942e2dc286a80b4c3e3156332de763ae4
Cameron Clough <cameronjclough@gmail.com>
2022-03-18 20:30:59 +0000

    remove broken script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,9 @@
 module.exports = [{
-  script: 'server.js',
   name: 'server',
+  script: 'server.js',
   node_args: '-r esm',
 }, {
+  name: 'worker',
   script: 'worker.js',
   node_args: '-r esm',
-  name: 'worker'
-}]
+}];
